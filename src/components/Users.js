67da5487b2b375ebab75d9e3018c0c5ebf81a711
Aminedroid <amine.ipod@gmail.com>
2024-03-30 20:15:56 +0100

    filter field improvment

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,42 +20,45 @@ const Users = () => {
 
     return (
         <div className="row side-row">
-            <InputGroup>
-                <FormControl onChange={(e) => setSearch(e.target.value)} placeholder="Search contacts"/>
-            </InputGroup>
+            <div className="col-3">
+                <InputGroup>
+                    <FormControl onChange={(e) => setSearch(e.target.value)} placeholder="Search users"/>
+                </InputGroup>
+            </div>
+            <p></p>
             <Table striped bordered hover>
-              <thead>
+                <thead>
                 <tr>
-                  <th>Id</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Age</th>
-                  <th>Gender</th>
-                  <th>City</th>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                    <th>City</th>
                 </tr>
-              </thead>
-              <tbody>
-              {users.filter((user) => {
-                  if (search.toLowerCase() === ''){
-                      return user;
-                  } else {
-                      return user.first_name.toLowerCase().includes(search) ||
-                          user.last_name.toLowerCase().includes(search) ||
-                          user.age.toString().includes(search) ||
-                          user.gender.toLowerCase().includes(search) ||
-                          user.city.toLowerCase().includes(search);
-                  }
-              }).map((user) =>
-                  <tr key={user.user_id}>
-                      <td>{user.user_id}</td>
-                      <td>{user.first_name}</td>
-                      <td>{user.last_name}</td>
-                      <td>{user.age}</td>
-                      <td>{user.gender}</td>
-                      <td>{user.city}</td>
-                  </tr>
-              )}
-              </tbody>
+                </thead>
+                <tbody>
+                {users.filter((user) => {
+                    if (search.toLowerCase() === '') {
+                        return user;
+                    } else {
+                        return user.first_name.toLowerCase().includes(search) ||
+                            user.last_name.toLowerCase().includes(search) ||
+                            user.age.toString().includes(search) ||
+                            user.gender.toLowerCase().includes(search) ||
+                            user.city.toLowerCase().includes(search);
+                    }
+                }).map((user) =>
+                    <tr key={user.user_id}>
+                        <td>{user.user_id}</td>
+                        <td>{user.first_name}</td>
+                        <td>{user.last_name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.gender}</td>
+                        <td>{user.city}</td>
+                    </tr>
+                )}
+                </tbody>
             </Table>
         </div>
     );
